dc19e07774c46aec07e937754308f45b6140a014
JotanVT <valikkudaththakkolkkaran.1759440@studenti.uniroma1.it>
2025-05-15 15:20:02 +0200

    errore risolto

--- emarket/db/migrate/20250512222834_create_segnalazione_negozios.rb
+++ emarket/db/migrate/20250512222834_create_segnalazione_negozios.rb
@@ -3,8 +3,8 @@ class CreateSegnalazioneNegozios < ActiveRecord::Migration[6.1]
     create_table :segnalazione_negozios do |t|
       t.string :motivo
       t.text :note
-      t.references :acquirente, null: false, foreign_key: true
-      t.references :negozio, null: false, foreign_key: true
+      t.references :acquirente, null: false, foreign_key:{on_delete: :cascade}
+      t.references :negozio, null: false, foreign_key:{on_delete: :cascade}
 
       t.timestamps
     end
